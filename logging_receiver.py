f9e8033f8ade4195d14ea8bdbdc29692bc83f006
Matt Mills <mmills@2bn.net>
2013-11-02 23:51:27 -0700

    bandwidth tweak

--- logging_receiver.py
+++ logging_receiver.py
@@ -25,7 +25,7 @@ class logging_receiver(gr.hier_block2):
 		
 		self.thread_id = 'logr-' + str(uuid.uuid4())
 		self.audiotaps = gr.firdes.low_pass( 1.0, self.samp_rate, (self.audio_rate/2), ((self.audio_rate/2)*0.6), firdes.WIN_HAMMING)
-		self.prefilter_decim = int(self.samp_rate/((self.audio_rate*1.4)))
+		self.prefilter_decim = int(self.samp_rate/((self.audio_rate*1.6)))
 		self.prefilter = gr.freq_xlating_fir_filter_ccc(self.prefilter_decim, self.audiotaps, 0, self.samp_rate)
 
 		#self.valve = gr_extras.stream_selector(gr.io_signature(1, 1, gr.sizeof_gr_complex), gr.io_signature(2, 2, gr.sizeof_gr_complex), )
