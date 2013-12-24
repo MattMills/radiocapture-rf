3c1cc4882bf6f1bc36b7b167528d62ee46970edd
Matt Mills <mmills@2bn.net>
2013-12-24 12:36:50 -0700

    No self

--- receiver.py
+++ receiver.py
@@ -152,7 +152,7 @@ if __name__ == '__main__':
 	print 'Entering top_block loop'
 
 	while 1:
-		self.tb.ar_lock.acquire()
+		tb.ar_lock.acquire()
 		for i,receiver in enumerate(tb.active_receivers):
 			if receiver.in_use == True and time.time()-receiver.time_activity > 3.5 and receiver.time_activity != 0 and receiver.time_open != 0:
 				receiver.close({})
@@ -161,6 +161,6 @@ if __name__ == '__main__':
 		#		tb.disconnect(tb.active_receivers[i])
 		#		del tb.active_receivers[i]
 		#		tb.unlock()
-		self.tb.ar_lock.release()
+		tb.ar_lock.release()
 		time.sleep(0.1)
 
