947dabc6336b0b7075a905d8baeb5b008a046e95
Matt Mills <mmills@2bn.net>
2015-09-05 21:51:19 -0700

    Update gains

--- configs/config_sdr102_bladerf.py
+++ configs/config_sdr102_bladerf.py
@@ -6,7 +6,8 @@
 # 4/28/2013 - Initial Creation
 class rc_config:
 	def __init__(self):
-		self.receiver_split2 = True	#Does the frontend receiver split each inbound source by 1/2
+		self.receiver_split2 = False	#Does the frontend receiver split each inbound source by 1/2
+		self.frontend_mode = 'xlat'
 
                 self.frontend_ip = '127.0.0.1'
                 self.backend_ip = '127.0.0.1'
@@ -16,20 +17,20 @@ class rc_config:
 			0:{
 				'type': 'bladerf',
 				#'args': 'bladerf=0,fpga=/home/mmills/build/hostedx40.rbf,buffers=4096,buflen=65536,transfers=15,stream_timeout_ms=30000,verbosity=debug',
-				'args': 'bladerf=0,fpga=/home/mmills/build/hostedx40.rbf',
-				'center_freq': 479693750,
+				'args': 'bladerf=0,fpga=/home/mmills/build/hostedx40.rbf,fpga-reload=1',
+				'center_freq': 470006250,
 				'samp_rate': 6000000,
-				'rf_gain': 2,
-				'bb_gain': 9
+				'rf_gain': 4,
+				'bb_gain': 30
 			},
 			1:{
                                 'type': 'bladerf',
                                 #'args': 'numchan=1 bladerf=1,fpga=/home/mmills/build/hostedx40.rbf,buffers=4096,buflen=65536,transfers=15,stream_timeout_ms=30000,verbosity=debug',
-				'args': 'bladerf=1,fpga=/home/mmills/build/hostedx40.rbf',
-                                'center_freq': 470006250,
+				'args': 'bladerf=1,fpga=/home/mmills/build/hostedx40.rbf,fpga-reload=1',
+				'center_freq': 479693750,
                                 'samp_rate': 6000000,
-                                'rf_gain': 2,
-                                'bb_gain': 9
+                                'rf_gain': 4,
+                                'bb_gain': 30
                         }
 		}
 		#del self.sources[1]
@@ -38,7 +39,7 @@ class rc_config:
 			2: { #ICIS P25 RFSS1 SITE10 - Pasadena
                                 'type': 'p25',
                                 'id': 1010,
-                                'default_control_channel': 0,
+                                'default_control_channel': 2,
 				'modulation': 'CQPSK',
                                 'channels': {
                                         0: 477012500,
