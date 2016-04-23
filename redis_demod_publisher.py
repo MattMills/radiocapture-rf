1f7c5a6e4378a582e39e8c208cec9a17762452ee
Matt Mills <mmills@2bn.net>
2016-04-23 04:53:44 -0600

    Initial work on p25 call manager, redis demod manager, and full work on redis demod publisher, including tying it into all the demods

---
+++ redis_demod_publisher.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import json
+import threading
+import time
+import redis
+
+class redis_demod_publisher():
+        def __init__(self, host=None, port=None, parent_demod=None):
+		
+		if(host != None):
+			self.host = host
+		else:
+			self.host = '127.0.0.1' #manually set here until there is a better place
+		if(port != None):
+			self.port = port
+		else:
+			self.port = 6379 #manually set here until there is a better place
+
+		if(parent_demod == None):
+			raise Exception('parent_demod cannot be null')
+
+		self.parent_demod = parent_demod
+
+		self.client = None
+		self.continue_running = True
+
+		self.init_connection()
+
+		publish_loop = threading.Thread(target=self.publish_loop)
+                publish_loop.daemon = True
+                publish_loop.start()
+	def init_connection(self):
+		self.client = redis.StrictRedis(host=self.host, port=self.port, db=0)
+
+	def publish_loop(self):
+		print 'publish_loop()'
+		while self.continue_running:
+			system_type = self.parent_demod.system['type']
+			
+
+			publish_data = {
+				'instance_uuid': self.parent_demod.instance_uuid,
+				'site_uuid': self.parent_demod.site_uuid,
+				'overseer_uuid': self.parent_demod.overseer_uuid,
+				'site_detail': self.parent_demod.site_detail,
+				'site_status': self.parent_demod.quality,
+				'auto_capture': True,
+				'frequency': self.parent_demod.control_channel,
+				'timestamp': time.time(),
+			}
+
+			
+			pipe = self.client.pipeline()
+			pipe.sadd('demod:%s' % system_type, self.parent_demod.instance_uuid)
+			pipe.set(self.parent_demod.instance_uuid, json.dumps(publish_data))
+			result = pipe.execute()
+			time.sleep(1)
+			
