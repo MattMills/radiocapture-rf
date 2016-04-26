738ee497c9e37dfae99cf0a241a1957993794b02
Matt Mills <mmills@2bn.net>
2016-04-26 16:16:59 -0600

    More work on p25 call manager, up to the point where its time to add call handling

--- p25_call_manager.py
+++ p25_call_manager.py
@@ -8,7 +8,11 @@ from stompest.protocol import StompSpec
 import json
 import threading
 import time
-import stompy
+import uuid
+import sys
+import signal
+
+from redis_demod_manager import redis_demod_manager
 
 class p25_call_manager():
         def __init__(self, host=None, port=None):
@@ -28,10 +32,21 @@ class p25_call_manager():
 
                 self.client = None
                 self.connection_issue = True
+		self.continue_running = True
+		self.subscriptions = {}
+
+
+		self.instance_metadata = {}
+		
 
                 connection_handler = threading.Thread(target=self.connection_handler)
                 connection_handler.daemon = True
                 connection_handler.start()
+
+		#time.sleep(0.25)
+		publish_loop = threading.Thread(target=self.publish_loop)
+		publish_loop.daemon = True
+		publish_loop.start()
 
         def init_connection(self):
                 if(self.client != None and self.client.session.state == 'connected'):
@@ -54,6 +69,36 @@ class p25_call_manager():
                                 except:
                                         pass
                         time.sleep(1)
+	def subscribe(self, queue):
+		#This needs to exist so we can keep track of what subs we have and re-sub on reconnect
+		if queue in self.subscriptions:
+			return True #we're already subscribed
+
+		if(self.connection_issue == True):
+                        return None	
+	
+		this_uuid = '%s' % uuid.uuid4()
+		try:
+			self.subscriptions[queue] = this_uuid
+			self.client.subscribe(queue, {StompSpec.ID_HEADER: this_uuid, StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL, })
+		except Exception as e:
+			print '%s' % e
+			self.connection_issue = True
+
+
+	def unsubscribe(self, queue):
+		if queue not in self.subscriptions:
+                        return False #cant unsubscribe, we're not subscribed
+
+                if(self.connection_issue == True):
+                        return None
+
+                try:
+                        self.client.unsubscribe(self.subscriptions[queue], {StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL})
+			del self.subscriptions[queue]
+                except Exception as e:
+			print '%s' % e
+                        self.connection_issue = True
 
         def send_event_lazy(self, destination, body):
                 #If it gets there, then great, if not, well we tried!
@@ -65,21 +110,92 @@ class p25_call_manager():
                 except:
                         self.connection_issue = True
 
+	def notify_demod_new(self, demod_instance_uuid):
+		print 'Notified of new demod %s' % (demod_instance_uuid)
+		self.subscribe('/topic/raw_control/%s' % (demod_instance_uuid))
+
+	def notify_demod_expire(self, demod_instance_uuid):
+		print 'Notified of expired demod %s' % (demod_instance_uuid)
+		self.unsubscribe('/topic/raw_control/%s' % (demod_instance_uuid))
+
+	def call_user_to_group(self, instance, channel, group_address, user_address=0):
+		print 'New Call %s %s %s %s' %(instance, channel, group_address, user_address)
+
 	def publish_loop(self):
 		print 'publish_loop()'
+		print '%s' % self.redis_demod_manager.demods
 		while self.continue_running:
+			if self.connection_issue == False:
+				try:
+					if not self.client.canRead(0.1):
+						print '-'
+						continue
+		        		frame = self.client.receiveFrame()
+				        t = json.loads(frame.body)
+					instance_uuid = frame.headers['destination'].replace('/topic/raw_control/', '')
+					instance = self.redis_demod_manager.demods[instance_uuid]
 
-			publish_data = {
-				'instance_uuid': self.parent_demod.instance_uuid,
-				'site_uuid': self.parent_demod.site_uuid,
-				'overseer_uuid': self.parent_demod.overseer_uuid,
-				'site_detail': self.parent_demod.site_detail,
-				'site_status': self.parent_demod.quality,
-				'auto_capture': True,
-				'frequency': self.parent_demod.control_channel,
-				'timestamp': time.time(),
-			}
+					if instance_uuid not in self.instance_metadata:
+                                                        self.instance_metadata[instance_uuid] = {'channel_identifier_table': {}}
 
-			
-			
-			time.sleep(1)
+					if 'crc' in t and t['crc'] != 0:
+						continue #Don't bother trying to work with bad data
+						
+                                        if t['name'] == 'IDEN_UP_VU':
+						try:
+	                                               self.instance_metadata[instance_uuid]['channel_identifier_table'][t['Identifier']] = {
+                                                        'BW': t['BW VU'],
+                                                        'Base Frequency': t['Base Frequency'],
+                                                        'Channel Spacing': t['Channel Spacing'],
+                                                        'Transmit Offset': t['Transmit Offset VU'],
+                                                        'Type': 'FDMA',
+                                                        'Slots': 1,
+                                                        }
+						except:
+							pass
+					elif t['name'] == 'IDEN_UP':
+						try:
+	                                               self.instance_metadata[instance_uuid]['channel_identifier_table'][t['Identifier']] = {
+                                                        'BW': t['BW'],
+                                                        'Base Frequency': t['Base Frequency'],
+                                                        'Channel Spacing': t['Channel Spacing'],
+                                                        'Transmit Offset': t['Transmit Offset'],
+                                                        'Type': 'FDMA',
+                                                        'Slots': 1,
+                                                        }
+						except:
+							pass
+					elif t['name'] == 'IDEN_UP_TDMA':
+						try:
+	                                                self.instance_metadata[instance_uuid]['channel_identifier_table'][t['Identifier']] = {
+                                                        'BW': t['BW'],
+                                                        'Base Frequency': t['Base Frequency'],
+                                                        'Channel Spacing': t['Channel Spacing'],
+                                                        'Transmit Offset': t['Transmit Offset TDMA'],
+                                                        'Type': t['Access Type'],
+                                                        'Slots': t['Slots'],
+                                                        }
+						except:
+							pass
+					elif t['name'] == 'GRP_V_CH_GRANT' :
+						self.call_user_to_group(instance_uuid, t['Channel'], t['Group Address'], t['Source Address'])
+					elif t['name'] == 'MOT_PAT_GRP_VOICE_CHAN_GRANT':
+						self.call_user_to_group(instance_uuid, t['Channel'], t['Super Group'], t['Source Address'])
+					elif(t['name'] == 'GRP_V_CH_GRANT_UPDT':
+						self.call_user_to_group(instance_uuid, t['Channel 0'], t['Group Address 0'])
+						self.call_user_to_group(instance_uuid, t['Channel 1'], t['Group Address 1'])
+					elif(t['name'] == 'MOT_PAT_GRP_VOICE_CHAN_GRANT_UPDT':
+                                                self.call_user_to_group(instance_uuid, t['Channel 0'], t['Super Group 0'])
+                                                self.call_user_to_group(instance_uuid, t['Channel 1'], t['Super Group 1'])
+
+				        self.client.ack(frame)
+				except Exception as e:
+					print 'except: %s' % e
+					raise
+					self.connection_issue = True
+
+if __name__ == '__main__':
+
+	main = p25_call_manager()
+	while True:
+		time.sleep(1)
