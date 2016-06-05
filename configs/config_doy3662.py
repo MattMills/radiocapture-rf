e8dfa2342d77e1f77b9a646ae5866e4f0a017864
Matt Mills <mmills@2bn.net>
2016-06-05 02:23:17 -0600

    Update config

--- configs/config_doy3662.py
+++ configs/config_doy3662.py
@@ -22,7 +22,7 @@ class rc_config:
 			0:{
 				'type': 'rtlsdr', 
 				'args': 'rtl=3-1,buffers=4',
-				'offset': 0,
+				'offset': 1040,
 				'bb_gain': self.if_gain,
 				'rf_gain': self.gain,
 				'center_freq': 770000000, 
@@ -31,7 +31,7 @@ class rc_config:
 			1:{
                                 'type': 'rtlsdr',
                                 'args': 'rtl=3-2,buffers=4',
-				'offset': 0,
+				'offset': 1350,
                                 'bb_gain': self.if_gain,
                                 'rf_gain': self.gain,
                                 'center_freq': 772000000,
@@ -40,10 +40,55 @@ class rc_config:
                         2:{
                                 'type': 'rtlsdr',
                                 'args': 'rtl=3-3,buffers=4',
-				'offset': 0,
+				'offset': 827,
                                 'bb_gain': self.if_gain,
                                 'rf_gain': self.gain,
                                 'center_freq': 774000000,
+                                'samp_rate': self.samp_rate
+                        },
+			3:{
+                                'type': 'rtlsdr',
+                                'args': 'rtl=3-4,buffers=4',
+                                'offset': 210,
+                                'bb_gain': 40,
+                                'rf_gain': self.gain,
+                                'center_freq': 851200000,
+                                'samp_rate': self.samp_rate
+                        },
+                        4:{
+                                'type': 'rtlsdr',
+                                'args': 'rtl=3-5,buffers=4',
+                                'offset': 460,
+                                'bb_gain': 40,
+                                'rf_gain': 40,
+                                'center_freq': 853400000,
+                                'samp_rate': self.samp_rate
+                        },
+                        5:{
+                                'type': 'rtlsdr',
+                                'args': 'rtl=3-6,buffers=4',
+                                'offset': 980,
+                                'bb_gain': 40,
+                                'rf_gain': 40,
+                                'center_freq': 855600000,
+                                'samp_rate': self.samp_rate
+                        },
+                        6:{
+                                'type': 'rtlsdr',
+                                'args': 'rtl=3-7,buffers=4',
+                                'offset': 0,
+                                'bb_gain': 40,
+                                'rf_gain': 40,
+                                'center_freq': 857800000,
+                                'samp_rate': self.samp_rate
+                        },
+                        7:{
+                                'type': 'rtlsdr',
+                                'args': 'rtl=3-8,buffers=4',
+                                'offset': 110,
+                                'bb_gain': 40,
+                                'rf_gain': 40,
+                                'center_freq': 860000000,
                                 'samp_rate': self.samp_rate
                         },
 			
@@ -55,7 +100,8 @@ class rc_config:
                         0: {
                                 'type': 'p25',
                                 'id': '1-001',
-                                'default_control_channel': 0,
+				'modulation': 'CQPSK',
+                                'default_control_channel': 15,
                                 'channels': {
 					0: 769256250,
 					1: 769268750,
@@ -83,6 +129,66 @@ class rc_config:
 					23: 774668750,
                                 }
                         },
+			#1: {
+			#	'type': 'moto',
+			#	'id': 0xa812,
+			#	'channels': {
+			#		10: 851262500,
+			#		25: 851637500,
+			#		35: 851887500,
+			#		50: 852262500,
+			#		110: 853762500,
+			#		#854962500
+			#		169: 855237500,
+			#		197: 855937500,
+			#		229: 856737500,
+			#		494: 852375000,
+			#	}
+			#}
+			#1: {	#NJCIS, hunterdon Simulcast
+			#	'type': 'p25',
+			#	'id': 'ngcis',
+			#	'modulation': 'C4FM',
+			#	'default_control_channel': 0,
+			#	'channels': {
+			#		0: 769206250,
+			#		1: 770193750,
+			#		2: 773031250,
+			#		3: 773593750,
+			#		4: 773831250,
+			#		5: 774818750,
+			#	}
+			#},
+			#2: {
+			#	'type': 'p25',
+			#	'id': 'peco',
+			#	'modulation': 'C4FM',
+			#	'default_control_channel': 0,
+			#	'channels': {
+			#		0: 854912500,
+			#		1: 856162500,
+			#		2: 857162500,
+			#		3: 858162500,
+			#		4: 859112500,
+			#		5: 860537500,
+			#	}
+			#},
+			#3: {
+			#	'type': 'moto',
+			#	'id': 'nj-sp',
+			#	'channels': {
+			#		0: 851012500,
+			#		48: 852212500,
+			#		85: 853137500,
+			#		228: 856712500,
+			#		248: 857212500,
+			#		268: 857717500,
+			#		288: 858212500,
+			#		308: 858712500,
+			#		328: 859212500,
+			#		348: 859712500,
+			#		368: 860212500,
+			#		388: 860712500,
+			#	}
+			#}
 		}
-
-		
