09c048348160ded2e61de6b9fa0723d9a29c3eb9
Matt Mills <mmills@2bn.net>
2016-09-14 03:00:50 -0600

    Change change to call continuation

--- p25_call_manager.py
+++ p25_call_manager.py
@@ -419,7 +419,8 @@ class p25_call_manager():
 
 								self.log.info('call_user_to_group %s %s %s %s' % (instance_uuid, channel, t['packet']['lc']['tgid'], t['packet']['lc']['source_id']))
 								if channel != -1:
-									self.call_user_to_group(instance_uuid, channel,t['packet']['lc']['tgid'], t['packet']['lc']['source_id'])
+									self.call_continuation(instance_uuid, channel, t['packet']['lc']['tgid'])
+									#self.call_user_to_group(instance_uuid, channel,t['packet']['lc']['tgid'], t['packet']['lc']['source_id'])
 							elif t['packet']['lc']['lcf_long'] == 'Group Voice Channel Update':
 								self.log.info('group voice channel update %s %s %s %s' % (t['packet']['lc']['channel_a'], t['packet']['lc']['channel_a_group'], t['packet']['lc']['channel_b'], t['packet']['lc']['channel_b_group']))
 								#self.call_user_to_group(instance_uuid, t['packet']['lc']['channel_a'] ,t['packet']['lc']['channel_a_group'], 0)
