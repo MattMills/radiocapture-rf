37e7b93d6f55a72a7f6cafb2c0fd663d9496837b
Matt Mills <mmills@2bn.net>
2022-04-28 22:09:40 +0000

    overseer.py: Only start call managers for demodulator types that are in use

--- overseer.py
+++ overseer.py
@@ -69,48 +69,56 @@ logger.info('Site UUID: %s' % site_uuid)
 
 
 demods = {}
+demod_type_count = {'p25': 0, 'edacs': 0, 'moto': 0}
+demod_types = {'p25': p25_control_demod, 'edacs': edacs_control_demod, 'moto': moto_control_demod}
+
 for x in config.systems:
         logger.info('Initializing %s demodulator. System configuration: %s' % (config.systems[x]['type'], config.systems[x]))
-        if config.systems[x]['type'] == 'edacs':
-                demods[x] = multiprocessing.Process(name='demod_edacs_%s' % (x, ), target=tb_worker, args=(edacs_control_demod, config.systems[x], site_uuid, overseer_uuid))
-        elif config.systems[x]['type'] == 'moto':
-                demods[x] = multiprocessing.Process(name='demod_moto_%s' % (x, ), target=tb_worker, args=(moto_control_demod, config.systems[x], site_uuid, overseer_uuid))
-        elif config.systems[x]['type'] == 'p25':
-                demods[x] = multiprocessing.Process(name='demod_p25_%s' % (x, ), target=tb_worker, args=(p25_control_demod, config.systems[x], site_uuid, overseer_uuid))
-                
+
+        demods[x] = multiprocessing.Process(name='demod_%s_%s' % (config.systems[x]['type'], x, ), target=tb_worker, args=(demod_types[config.systems[x]['type']], config.systems[x], site_uuid, overseer_uuid))
+        demod_type_count[config.systems[x]['type']] += 1
+
         demods[x].start()
         logger.info('demodulator %s pid: %s' % (x, demods[x].pid))
 
 import time
 
 logger.info('Initializing call managers')
-p25_cm = multiprocessing.Process(name='p25_cm', target=worker, args=(p25_call_manager,))
-p25_cm.start()
-logger.info('p25_cm pid  %s' % p25_cm.pid)
-moto_cm = multiprocessing.Process(name='moto_cm', target=worker, args=(moto_call_manager,))
-moto_cm.start()
-logger.info('moto_cm pid  %s' % moto_cm.pid)
-edacs_cm = multiprocessing.Process(name='edacs_cm', target=worker, args=(edacs_call_manager,))
-edacs_cm.start()
-logger.info('edacs_cm pid  %s' % edacs_cm.pid)
 
-#p25_md_agent = multiprocessing.Process(target=worker, args=(p25_metadata_agent,))
-#p25_md_agent.start()
+if(demod_type_count['p25'] > 0):
+    p25_cm = multiprocessing.Process(name='p25_cm', target=worker, args=(p25_call_manager,))
+    p25_cm.start()
+    logger.info('p25_cm pid  %s' % p25_cm.pid)
+
+    #p25_md_agent = multiprocessing.Process(target=worker, args=(p25_metadata_agent,))
+    #p25_md_agent.start()
+if(demod_type_count['moto'] > 0):
+    moto_cm = multiprocessing.Process(name='moto_cm', target=worker, args=(moto_call_manager,))
+    moto_cm.start()
+    logger.info('moto_cm pid  %s' % moto_cm.pid)
+if(demod_type_count['edacs'] > 0):
+    edacs_cm = multiprocessing.Process(name='edacs_cm', target=worker, args=(edacs_call_manager,))
+    edacs_cm.start()
+    logger.info('edacs_cm pid  %s' % edacs_cm.pid)
+
 
 logger.info('Initializing call recorder manager')
-
 
 call_recorder = multiprocessing.Process(name='call_recorder', target=worker, args=(call_recorder_manager,))
 call_recorder.start()
 logger.info('call_recorder pid  %s' % call_recorder.pid)
 
 logger.info('Overseer %s initialization complete' % overseer_uuid)
+
 while 1:
         for demod in demods.keys():
             logger.info('Overseer thread status (demod, %s %s): is_alive(%s)' % (demod, demods[demod].pid, demods[demod].is_alive()))
-        logger.info('Overseer thread status (p25_cm, %s): is_alive(%s)' % (p25_cm.is_alive(),p25_cm.pid))
-        logger.info('Overseer thread status (moto_cm, %s): is_alive(%s)' % (moto_cm.is_alive(), moto_cm.pid))
-        logger.info('Overseer thread status (edacs_cm, %s): is_alive(%s)' % (edacs_cm.is_alive(), edacs_cm.pid))
+        if(demod_type_count['p25'] > 0):
+            logger.info('Overseer thread status (p25_cm, %s): is_alive(%s)' % (p25_cm.is_alive(),p25_cm.pid))
+        if(demod_type_count['moto'] > 0):
+            logger.info('Overseer thread status (moto_cm, %s): is_alive(%s)' % (moto_cm.is_alive(), moto_cm.pid))
+        if(demod_type_count['edacs'] > 0):
+            logger.info('Overseer thread status (edacs_cm, %s): is_alive(%s)' % (edacs_cm.is_alive(), edacs_cm.pid))
         logger.info('Overseer thread status (call_recorder_manager, %s): is_alive(%s)' % (call_recorder.is_alive(), call_recorder.pid))
 
         time.sleep(5)
